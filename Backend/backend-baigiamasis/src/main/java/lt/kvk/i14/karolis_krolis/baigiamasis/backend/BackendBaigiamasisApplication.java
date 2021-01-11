169851ace040e806bfc3a0bd4112106f5c406d3b
Kkrolis <kkrolis123@gmail.com>
2021-01-11 11:54:00 +0200

    Backend initial commit

---
+++ Backend/backend-baigiamasis/src/main/java/lt/kvk/i14/karolis_krolis/baigiamasis/backend/BackendBaigiamasisApplication.java
@@ -0,0 +1,13 @@
+package lt.kvk.i14.karolis_krolis.baigiamasis.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBaigiamasisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBaigiamasisApplication.class, args);
+	}
+
+}
---
+++ Backend/backend-baigiamasis/src/test/java/lt/kvk/i14/karolis_krolis/baigiamasis/backend/BackendBaigiamasisApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.kvk.i14.karolis_krolis.baigiamasis.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBaigiamasisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
