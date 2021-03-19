69e3a476333e889c44cbc053149d3a3d45621b5e
Karolis <kkrolis123@gmail.com>
2021-03-19 10:11:29 +0200

    If pass or srn invalid clears password field

--- Frontend/src/app/security/security.component.ts
+++ Frontend/src/app/security/security.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { JwtClientService } from '../jwt-client.service';
-import {Router,ActivatedRoute} from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { InvalidLoginComponent } from '../dialogs/invalid-login/invalid-login.component';
 
@@ -13,13 +13,15 @@ import { InvalidLoginComponent } from '../dialogs/invalid-login/invalid-login.co
 export class SecurityComponent implements OnInit {
 
   authRequest: any =
-  {
-    "userName": null,
-    "userPassword": null
-  }
+    {
+      "userName": null,
+      "userPassword": null
+    }
 
-  response:any;
-  token:any;
+  response: any;
+  token: any;
+
+  inputPassword: string;
 
   constructor(private service: JwtClientService, private readonly router: Router, private readonly route: ActivatedRoute, private dialog: MatDialog) { }
 
@@ -27,16 +29,16 @@ export class SecurityComponent implements OnInit {
 
   }
 
-  public login(userName, userPassword){
+  public login(userName, userPassword) {
     this.authRequest.userName = userName;
     this.authRequest.userPassword = userPassword;
     this.getAccessTokenAndLogin(this.authRequest);
 
   }
 
-  public getAccessTokenAndLogin(authRequest){
+  public getAccessTokenAndLogin(authRequest) {
     let res = this.service.generateToken(authRequest);
-    res.subscribe(data=>{
+    res.subscribe(data => {
       this.accessApi(data); // this is only for testing porpuses
       this.token = data;
       if (data == "Invalid username or password") {
@@ -44,24 +46,25 @@ export class SecurityComponent implements OnInit {
       } else {
         localStorage.setItem('token', this.token);
       }
-      this.router.navigate(['/main_page'], {relativeTo: this.route});  // navigates to another route, if used in antoher place doesn't work
+      this.router.navigate(['/main_page'], { relativeTo: this.route });  // navigates to another route, if used in antoher place doesn't work
     });
   }
 
-// this for testing saving to local storage
-  public accessApi(token){
+  // this for testing saving to local storage
+  public accessApi(token) {
     let res = this.service.welcome(token);
-    res.subscribe(data=>{
+    res.subscribe(data => {
       this.response = data;
       localStorage.setItem('userName', this.response);
     });
   }
 
-  public toRegistracion(){
-    this.router.navigate(['/registration'], {relativeTo: this.route});
+  public toRegistracion() {
+    this.router.navigate(['/registration'], { relativeTo: this.route });
   }
 
-  public openInvalidUserDialog () {
+  public openInvalidUserDialog() {
+    this.inputPassword = '';
     const dialogConfig = new MatDialogConfig();
 
     dialogConfig.disableClose = true;
