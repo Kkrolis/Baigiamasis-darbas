04eef99d654e6c8633f6e786ff00f31e27b4f80d
Karolis <kkrolis123@gmail.com>
2021-03-19 10:36:32 +0200

    updated interes calculator
    updated calculator the every interaction with it recalculates result

--- Frontend/src/app/interest-calc/interest-calc.component.ts
+++ Frontend/src/app/interest-calc/interest-calc.component.ts
@@ -12,11 +12,15 @@ export class InterestCalcComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-
+    this.result = 0;
+  }
+  
+  changeResult (moneyAmmountToBarrow: number, interestRate1: number, interesCompoundPerPeriod: number, forHowLongLoaned: number) {
+    this.calculateCompoundInterest(moneyAmmountToBarrow, interestRate1, interesCompoundPerPeriod, forHowLongLoaned);
   }
 
   public calculateCompoundInterest(moneyAmmountToBarrow: number, interestRate1: number, interesCompoundPerPeriod: number, forHowLongLoaned: number) {
-    console.log(moneyAmmountToBarrow);
+    //console.log(moneyAmmountToBarrow);
     let interestRate = interestRate1 / 100;
     let total;
     let calculation1: number = 1 + interestRate / interesCompoundPerPeriod;
@@ -24,9 +28,9 @@ export class InterestCalcComponent implements OnInit {
     let calculation3: number = Math.pow(calculation1, calculation2);
     total = moneyAmmountToBarrow * calculation3;
     total = (Math.round(total * 100) / 100).toFixed(2);
-    console.log(total);
+    //console.log(total);
     this.result = total;
-    console.log(isNaN(this.result));
+    //console.log(isNaN(this.result));
   }
 
 }
