3eb384e89f8cc5cb3c933cf38a18fb4b394f40a0
Karolis <kkrolis123@gmail.com>
2021-04-09 12:41:23 +0300

    fxed loan post bugs
    fixed bug that not displayed loan reasons, and added ensurance that loan reason is allways provided

--- Backend/backend-baigiamasis/src/main/java/lt/kvk/i14/karolis_krolis/baigiamasis/backend/api/controller/LoanPostController.java
+++ Backend/backend-baigiamasis/src/main/java/lt/kvk/i14/karolis_krolis/baigiamasis/backend/api/controller/LoanPostController.java
@@ -1,24 +1,35 @@
 package lt.kvk.i14.karolis_krolis.baigiamasis.backend.api.controller;
 
 import lt.kvk.i14.karolis_krolis.baigiamasis.backend.api.entity.LoanPost;
-import lt.kvk.i14.karolis_krolis.baigiamasis.backend.api.entity.User;
+import lt.kvk.i14.karolis_krolis.baigiamasis.backend.api.entity.LoanReason;
 import lt.kvk.i14.karolis_krolis.baigiamasis.backend.api.repository.LoanPostRepository;
+import lt.kvk.i14.karolis_krolis.baigiamasis.backend.api.repository.LoanReasonRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/loanPost")
 public class LoanPostController {
 
     private final LoanPostRepository repository;
+    private final LoanReasonRepository reasonRepository;
 
-    public LoanPostController(LoanPostRepository repository) {this.repository = repository;}
+    public LoanPostController(LoanPostRepository repository, LoanReasonRepository reasonRepository) {this.repository = repository;
+        this.reasonRepository = reasonRepository;
+    }
 
     @PostMapping("/addPost")
     LoanPost addLoanPost (@RequestBody LoanPost loanPost) {
         System.out.println("this is working");
-        return repository.save((loanPost));
+        if (loanPost.getReason() != null) {
+            return repository.save((loanPost));
+        } else {
+            loanPost.setReason(reasonRepository.findById(1).get());
+            return repository.save(loanPost);
+        }
     }
 
     @GetMapping("/")
--- Frontend/src/app/loan-posts-page/loan-posts-page.component.ts
+++ Frontend/src/app/loan-posts-page/loan-posts-page.component.ts
@@ -16,11 +16,28 @@ export class LoanPostsPageComponent implements OnInit {
   constructor(public service: LoanPostService) { }
 
   ngOnInit(): void {
-    this.service.getPosts()
-      .subscribe((respose: Response) => {
-        //console.log(data);
-        this.posts = respose;
-      });
+    // this.service.getPosts()
+    //   .subscribe((respose: Response) => {
+    //     //console.log(data);
+    //     this.posts = respose;
+    //   });
+    this.getposts();
+  }
+
+  async getposts() {
+    await this.service.getPosts()
+    .subscribe((respose: Response) => {
+      //console.log(data);
+      this.posts = respose;
+    });
+  }
+  
+  getReasonName(row: number) {
+    // console.log(reason);
+    if (this.posts[row].reason.reasonName !== null) {
+      return String(this.posts[row].reason.reasonName);
+    }
+    return "Ivyko klaida";
   }
 
   
