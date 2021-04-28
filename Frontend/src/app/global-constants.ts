1f694e46630b54b9d408dcba53c61e2654bd5c83
Karolis <kkrolis123@gmail.com>
2021-04-28 19:38:49 +0300

    add heroku deploy

--- Frontend/src/app/global-constants.ts
+++ Frontend/src/app/global-constants.ts
@@ -1,4 +1,5 @@
 export class GlobalConstants {
-    public static apiURL: string = "http://localhost:8080/api";
-    public static webSocketUrl: string = "ws://localhost:8080/api"
+    // public static apiURL: string = "http://localhost:8080/api";
+    public static apiURL: string = "https://pacific-gorge-26209.herokuapp.com/api";
+    public static webSocketUrl: string = "ws://pacific-gorge-26209.herokuapp.com/api";
 }
